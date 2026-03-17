47cfeb09ee58da0799f2daf15e46148f40a76fe8
Lucas Rasia <lucasrasia@outlook.com>
2026-03-17 19:45:44 -0300

    Atividade 3
    Add input validation for single-digit integers and calculate concatenated value and its square.

--- 03.py
+++ 03.py
@@ -1,1 +1,15 @@
+ #Atividade 03: Concatenação inteira
 
+numero1=input('')
+numero2=input('')
+numero3=input('')
+if int(numero1)>9:
+  print('Dígito Inválido')
+elif int(numero2)>9:
+  print('Dígito Inválido')
+elif int(numero3)>9:
+  print('Dígito Inválido')
+else:
+  conta=(int(numero1)*100)+(int(numero2)*10)+(int(numero3))
+  conta2=conta**2
+  print(f'{conta}, {conta2}')
