5acdea27832b51f060186aa38b71d14941683c16
Lucas Rasia <lucasrasia@outlook.com>
2026-03-17 19:42:42 -0300

    Atividade 7

---
+++ 07.py
@@ -0,0 +1,7 @@
+ #Atividade 07: Conversor de unidades
+F=float(input('Valor em Fahrenheit: '))
+P=float(input('Valor em polegada: '))
+C=(5*F-160)/9
+M=P*25.4
+print(f'Valor em Celsius: {C:.2f}')
+print(f'Valor em milimetros: {M}')
