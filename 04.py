d5fc12056b9578950ce956fbdeeab34f7cad45da
Lucas Rasia <lucasrasia@outlook.com>
2026-03-17 19:44:54 -0300

    Atividade 4

--- 04.py
+++ 04.py
@@ -1,1 +1,9 @@
+salario=float(input('Salário miínimo: '))
+kw=float(input('Quantidade de kw gasto: '))
+ckw=salario*0.007  
+cc=kw*ckw
+cd=cc*0.9
+print(f'Custo por kW: R$ {ckw:.2f}')
+print(f'Custo do consumo: R$ {cc:.2f}')
+print(f'Custo com desconto: R$ {cd:.2f}')
 
