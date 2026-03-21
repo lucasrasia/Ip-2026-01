6fcdd4e2e6544dccf658eff5533e9bf1aebf40ec
Lucas Rasia <lucasrasia@outlook.com>
2026-03-20 23:30:26 -0300

    Add script to convert time to seconds

---
+++ 20.py
@@ -0,0 +1,5 @@
+h=float(input('Quantidade de horas: '))
+m=float(input('Quantidade de minutos: '))
+s=float(input('Quantidade de segundos: '))
+soma= h*3600+m*60+s
+print(f'Tempo em segundos: {int(soma)}')
