a000df406e731508998c781b7f910eeb039a4aa8
Lucas Rasia <lucasrasia@outlook.com>
2026-03-26 22:17:18 +0000

    02 feito

--- Lista 2/02/02.go
+++ Lista 2/02/02.go
@@ -2,5 +2,14 @@ package main
 import("fmt")
 
 func main(){
-	
+	var n int
+	fmt.Print("Número: ")
+	fmt.Scan(&n)
+	if n>0 {
+		fmt.Printf("%v é positivo \n", n)		
+	} else if n<0 {
+		fmt.Printf("%v é negativo \n", n)
+	} else {
+		fmt.Printf("%v é nulo \n", n)
+	}
 }
