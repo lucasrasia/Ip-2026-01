2593252e5d8f4d1076f2345f1a9a86f879402ba5
Lucas Rasia <lucasrasia@outlook.com>
2026-03-26 18:55:39 +0000

    01 lista 2

--- Lista 2/01/01.go
+++ Lista 2/01/01.go
@@ -2,7 +2,12 @@ package main
 import("fmt")
 
 func main(){
-	x:=7
-	y:=9
-	fmt.Println(x>y)
+	var n int
+	fmt.Print("Digíte o número: ")
+	fmt.Scan(&n)
+	if n%2==0 {
+		fmt.Printf("%v é par\n", n)
+	}else {
+		fmt.Printf("%v é ímpar\n", n)
+	}
 }
