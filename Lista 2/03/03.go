22415b6165a94ac726ce72618f7a9d965da64bad
Lucas Rasia <lucasrasia@outlook.com>
2026-03-27 00:14:32 +0000

    03 (não testei ainda)

---
+++ Lista 2/03/03.go
@@ -0,0 +1,17 @@
+package main
+import("fmt")
+
+func main(){
+	var n1 int
+	var n2 int
+	fmt.Print("Número 1: ")
+	fmt.Scan(%n1)
+	fmt.Print("Número 2: ")
+	fmt.Scan(%n2)
+	n=n1+n2
+	if n>20 {
+		fmt.Println(n+8)
+	} else {
+		fmt.Println(n-5)
+	}
+}
