82f5cf86be8e1ef2330639371dbf6062a06c9582
Lucas Rasia <lucasrasia@outlook.com>
2026-03-28 12:04:46 +0000

    08 feito

--- Lista 2/08/08.go
+++ Lista 2/08/08.go
@@ -1,1 +1,13 @@
-package 
+package main
+import ("fmt")
+
+func main(){
+	var n float64
+	fmt.Print("Digíte um número: ")
+	fmt.Scan(&n)
+	if 20<n && n<90 {
+		fmt.Printf("%v está entre 20 e 90 \n", n)
+	} else{
+		fmt.Printf("%v não está entre 20 e 90 \n", n)
+	}
+}
