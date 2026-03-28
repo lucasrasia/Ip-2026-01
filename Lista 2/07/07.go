6bbce83187a096e0542b7b2c92b6288fdef2493c
Lucas Rasia <lucasrasia@outlook.com>
2026-03-28 11:58:14 +0000

    07 feito

--- Lista 2/07/07.go
+++ Lista 2/07/07.go
@@ -2,5 +2,13 @@ package main
 import "fmt"
 
 func main() {
-	
+	var a, b, c int	
+	fmt.Print("Digite 3 valores inteiros: ")
+	fmt.Scan(&a, &b, &c)
+	mi:=min(a, b, c)
+	ma:=max(a, b, c)
+	fmt.Println("Mínimo:", mi)
+	i:= a+b+c-mi-ma
+	fmt.Println("Intermediário:", i)
+	fmt.Println("Máximo:", ma)
 }
