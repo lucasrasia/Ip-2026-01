e9f82e97115253c7f3901064ed30fa1e1f6319ef
Lucas Rasia <lucasrasia@outlook.com>
2026-03-30 21:42:34 -0300

    Add program to input and display grades in reverse

---
+++ aula 06/Exercício 03/03.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main() {
+    var nota[10]float64
+    for i:=0; i<10; i++ {
+        fmt.Printf("Nota %v: ", (i+1))
+        fmt.Scan(&nota[i])
+    }
+    for i := range nota {
+        fmt.Printf("%v\n", nota[(9-i)])
+    }
+}
