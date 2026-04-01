30ac8356646f8c8cc09e808c0d6d34aa0db5a8d6
Lucas Rasia <lucasrasia@outlook.com>
2026-04-01 22:52:04 +0000

    01

--- aula 07/Exercício 01/01.go
+++ aula 07/Exercício 01/01.go
@@ -1,1 +1,14 @@
 package main
+import"fmt"
+func soma(a, b float64)float64{ //soma é o nome, no return eu defino o que a função vai fazer
+		return a+b
+	}
+func main() {
+	var a, b float64  //precisa definir a variavel
+	fmt.Print("Valor 1: ")
+	fmt.Scan(&a)
+	fmt.Print("Valor 2: ")
+	fmt.Scan(&b)
+	s:=soma(a, b)
+	fmt.Println("A soma é: ", s)
+}
