f4f8df85057a18e9a1e88aeaf2d6ba8b91062a0f
Lucas Rasia <lucasrasia@outlook.com>
2026-04-01 23:39:59 +0000

    04

---
+++ aula 07/04/04.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+func fat(n int)int{
+	 if n <= 1 {
+        return 1
+    }
+    return n * fat(n-1)
+}
+func main(){
+	var n int
+	fmt.Print("Número: ")
+	fmt.Scan(&n)
+	f:=fat(n)
+	fmt.Printf("%v fatorial é %v\n", n, f)
+}
