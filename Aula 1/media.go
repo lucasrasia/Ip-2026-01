76fd609943e463a538e8dbb1558f88cc7fe659a2
Lucas Rasia <lucasrasia@outlook.com>
2026-04-01 23:55:20 +0000

    n

--- Aula 1/media.go
+++ Aula 1/media.go
@@ -1,17 +1,16 @@
 package main
 import ("fmt")
-
+func verificar(n int) {
+    if n < 0 {
+        fmt.Println("Número negativo!")
+        return  // sai da função sem retornar valor
+    }
+    fmt.Println("Número válido")
+}
 func main(){
 	var n int
-	var nota float32
-	var soma float32
-	fmt.Print("Número de alunos: ")
+	fmt.Print(" ")
 	fmt.Scan(&n)
-	for i:=1; i < (n+1); i++ {
-		fmt.Printf("Nota %v: ", i)
-		fmt.Scan(&nota)
-		soma+=nota
-	}
-	media:=(soma)/float32(n)
-	fmt.Printf("Média: %.2f \n", media)
+	f:=verificar(n)
+	fmt.Println(f)
 }
--- aula 07/04/04.go
+++ aula 07/04/04.go
@@ -1,10 +1,10 @@
 package main
 import "fmt"
 func fat(n int)int{
-	 if n <= 1 {
-        return 1
-    }
-    return n * fat(n-1)
+	 if n==0 || n==1{
+		return 1
+	 }
+	 return n*fat(n-1)
 }
 func main(){
 	var n int
