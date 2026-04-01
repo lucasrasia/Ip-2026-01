ab21fbd49874bc1fe462dbec2e123a9afb5152fd
Lucas Rasia <lucasrasia@outlook.com>
2026-04-01 23:03:33 +0000

    03

--- aula 07/03/03.go
+++ aula 07/03/03.go
@@ -1,1 +1,12 @@
-package 
+package main
+import "fmt"
+func media(a, b, c float64)float64{
+	return (a+b+c)/3
+}
+ func main(){
+	var a, b, c float64
+	fmt.Print("Digíte 3 valores: ")
+	fmt.Scan(&a, &b, &c)
+	m:=media(a, b, c)
+	fmt.Printf("A média é: %.2f\n", m)
+ }
