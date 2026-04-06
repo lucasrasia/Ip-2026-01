419b998dd2310e366c48a2934816a6bc5674e9f8
Lucas Rasia <lucasrasia@outlook.com>
2026-04-06 17:35:24 +0000

    15 (ver solucao melhor)

--- Lista 2/15/15.go
+++ Lista 2/15/15.go
@@ -1,1 +1,10 @@
-package 
+package main
+import "fmt"
+
+func main(){
+	mes:=[12]string{"janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"}
+	var d, m, a int
+	fmt.Print("Coloque uma data(dd mm aaaa): ")
+	fmt.Scan(&d, &m, &a)
+	fmt.Printf("%v de %v de %v \n", d, mes[m-1], a)
+}
