ead8e70718f5a579cb0bdd19c4a7cfb3a156ff41
Lucas Rasia <lucasrasia@outlook.com>
2026-04-06 21:51:25 +0000

    corrige funções de cálculo de raízes e melhora a lógica de verificação de delta

--- Lista 2/16/16.go
+++ Lista 2/16/16.go
@@ -3,14 +3,25 @@ import (
 	"fmt"
 	"math"
 )
+var a, b, c float64
+var delta float64
 func bask1(a, b, c float64)float64{
-	return (math.Sqrt(b*b-4*a*c)-b)/2*a
+	return (-b + math.Sqrt(delta)) / (2 * a)
 }
-func baks2(a, b, c float64)float64{
-	return (-b-(math.Sqrt(b*b-4*a*c)))/2*a
+func bask2(a, b, c float64)float64{
+	return (-b - math.Sqrt(delta)) / (2 * a)
 }
 func main(){
-	var a, b, c float64
 	fmt.Print("Valores de A, B e C: ")
 	fmt.Scan(&a, &b, &c)
+	delta = b*b - 4*a*c
+	b1:= bask1(a, b, c)
+	b2:= bask2(a, b, c)
+	if delta<0{
+		fmt.Println("As raízes sao imagnárias, não há como expressar com números reais")
+	} else if delta==0{
+		fmt.Printf("Ás raízes são iguais: %.2f \n", b1)
+	} else{
+		fmt.Printf("Ás raízes são: %.2f e %.2f \n", b1, b2)
+	}
 }
