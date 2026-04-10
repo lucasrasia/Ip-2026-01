85c775bb6f6fd0c0a2470e59a33b05bee8ae359d
Lucas Rasia <lucasrasia@outlook.com>
2026-04-10 22:14:28 +0000

    01 feito

--- Lista 3/01/01.go
+++ Lista 3/01/01.go
@@ -1,5 +1,14 @@
 package main
 import "fmt"
 func main(){
-	var a, b
+	var a, b int
+	r:=1
+	fmt.Print("Digíte a base: ")
+	fmt.Scan(&a)
+	fmt.Print("Dígite o expoente: ")
+	fmt.Scan(&b)
+	for i:=1; i<=b; i++{
+		r=r*a
+	}
+	fmt.Printf("Resultado: %v\n", r)
 }
