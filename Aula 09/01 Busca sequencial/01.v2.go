06c718a6536ab4af63947ed039628c0494b22cc6
Lucas Rasia <lucasrasia@outlook.com>
2026-04-14 00:15:49 +0000

    01.v2 busca sequencial

--- Aula 09/01 Busca sequencial/01.v2.go
+++ Aula 09/01 Busca sequencial/01.v2.go
@@ -2,7 +2,7 @@ package main
 import "fmt"
 func busca(banco[]int, v int)int{
 	for _, i:=range banco{
-		if banco[i]== n{
+		if banco[i]==v{
 			return 1
 		}
 	}
@@ -10,11 +10,17 @@ func busca(banco[]int, v int)int{
 }
 func main(){
 	n := 9
-	array := make([]int, n)
+	banco := make([]int, n)
 	for i := 0; i < n; i++ {
-    array[i] = i + 1 // gera: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    banco[i] = i + 1 // gera: [1, 2, 3, 4, 5, 6, 7, 8, 9]
 }	
 	var v int
 	fmt.Print("Digite o valor: ")
 	fmt.Scan(&v)
+	b:=busca(banco, v)
+	if b!=-1{
+		fmt.Printf("%v está no banco de dados\n", v)
+	} else{
+		fmt.Printf("%v não está no banco de dados\n", v)
+	}
 }
