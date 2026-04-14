d8b787b40384ca95c93fcd7c8b25bdaa3710026b
Lucas Rasia <lucasrasia@outlook.com>
2026-04-14 18:26:20 -0300

    06 feito
    Added logic to check if a number is triangular.

--- Lista 3/06/06.go
+++ Lista 3/06/06.go
@@ -1,5 +1,19 @@
 package main
 import "fmt"
-func main(){
-	
+
+func main() {
+  var n int
+  fmt.Print("Digite um número: ")
+  fmt.Scan(&n)
+  e_triangular:=false
+  for i:=3; i<=n; i++ {
+      if i*(i-1)*(i-2)==n{
+          e_triangular=true
+      }
+  }
+  if e_triangular{
+      fmt.Printf("%v é triangular", n)
+  } else{
+      fmt.Print("%v não é triangular", n)
+  }
 }
